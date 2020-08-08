cc73958695c6499a6254380279fa9d779debca82
reyeon1209 <reyeon5368@naver.com>
2020-08-08 19:22:32 +0900

    200808 Refactoring

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body, control) {
+var template = {
+  HTML : function(title, list, body, control) {
     return `
     <!doctype html>
     <html>
@@ -19,16 +20,18 @@ function templateHTML(title, list, body, control) {
     </body>
     </html>
     `;
-}
+  },
 
-function templateList(filelist) {
+  list : function(filelist) {
     var list = '<ul>';
     filelist.forEach(element => list += `<li><a href="/?id=${element}">${element}</a></li>`);
     list += '</ul>';
 
     return list;
+  }
 }
- 
+
+
 var app = http.createServer(function(request,response) {  // request : 요청할 때 정보들, response : 응답할 때 정보들
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -39,18 +42,18 @@ var app = http.createServer(function(request,response) {  // request : 요청할
           fs.readdir('./data', (error, filelist) => {
             var title = 'Welcome';
             var description = 'Hello, Node.js';
-            var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
+            var list = template.list(filelist);
+            var html = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
 
             response.writeHead(200);  // 200 : 파일을 성공적으로 전송
-            response.end(template);
+            response.end(html);
           });
       } else {
         fs.readdir('./data', (error, filelist) => {
             fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) =>  {
             var title = queryData.id;
-            var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,
+            var list = template.list(filelist);
+            var html = template.HTML(title, list, `<h2>${title}</h2>${description}`,
             `
               <a href="/create">create</a>
               <a href="/update?id=${title}">update</a>
@@ -61,15 +64,15 @@ var app = http.createServer(function(request,response) {  // request : 요청할
             `);
 
             response.writeHead(200);  // 200 : 파일을 성공적으로 전송
-            response.end(template);
+            response.end(html);
             });
         });
       }
     } else if (pathname === '/create') {
         fs.readdir('./data', (error, filelist) => {
           var title = 'WEB - create';
-          var list = templateList(filelist);
-          var template = templateHTML(title, list,
+          var list = template.list(filelist);
+          var html = template.HTML(title, list,
             `
               <form action="/create_process" method="post">
                 <p><input type="text" name="title" placeholder="title"></p>
@@ -83,7 +86,7 @@ var app = http.createServer(function(request,response) {  // request : 요청할
             `, '');
 
           response.writeHead(200);  // 200 : 파일을 성공적으로 전송
-          response.end(template);
+          response.end(html);
         });
     } else if (pathname === '/create_process') {
       var body = '';
@@ -108,8 +111,8 @@ var app = http.createServer(function(request,response) {  // request : 요청할
       fs.readdir('./data', (error, filelist) => {
         fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) =>  {
         var title = queryData.id;
-        var list = templateList(filelist);
-        var template = templateHTML(title, list,
+        var list = template.list(filelist);
+        var html = template.HTML(title, list,
           `
             <form action="/update_process" method="post">
               <input type="hidden" name="id" value="${title}">
@@ -124,7 +127,7 @@ var app = http.createServer(function(request,response) {  // request : 요청할
           `, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
 
         response.writeHead(200);  // 200 : 파일을 성공적으로 전송
-        response.end(template);
+        response.end(html);
         });
     });
   } else if (pathname === '/update_process') {
