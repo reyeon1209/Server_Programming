715ed1fc58b500ed22a31fefcde527b5a60c810f
reyeon1209 <reyeon5368@naver.com>
2020-08-09 23:26:41 +0900

    글생성

--- Nodejs_MySQL/main.js
+++ Nodejs_MySQL/main.js
@@ -37,30 +37,6 @@ var app = http.createServer(function(request,response){
           response.end(html);
         });
       } else {
-        /*
-        fs.readdir('./data', function(error, filelist){
-          var filteredId = path.parse(queryData.id).base;
-          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-            var title = queryData.id;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-              allowedTags:['h1']
-            });
-            var list = template.list(filelist);
-            var html = template.HTML(sanitizedTitle, list,
-              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-              ` <a href="/create">create</a>
-                <a href="/update?id=${sanitizedTitle}">update</a>
-                <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
-                  <input type="submit" value="delete">
-                </form>`
-            );
-            response.writeHead(200);
-            response.end(html);
-          });
-        });
-        */
        db.query(`SELECT * FROM topic`, function (error, results) {
           if (error) {
             throw error;
@@ -88,10 +64,14 @@ var app = http.createServer(function(request,response){
         });
       }
     } else if(pathname === '/create'){
-      fs.readdir('./data', function(error, filelist){
-        var title = 'WEB - create';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list, `
+       db.query(`SELECT * FROM topic`, function (error, results) {
+        if (error) {
+          throw error;
+        }
+        var title = 'Create';
+        var list = template.list(results);
+        var html = template.HTML(title, list,
+          `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
@@ -101,7 +81,10 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
           </form>
-        `, '');
+          `,
+          `<a href="/create">create</a>`
+        );
+
         response.writeHead(200);
         response.end(html);
       });
@@ -112,12 +95,15 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var title = post.title;
-          var description = post.description;
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
+          db.query(`INSERT INTO topic (title, description, created, author_id)
+          VALUES(?, ?, NOW(), ?)`, [post.title, post.description, 1]
+          , function (error, result) {
+            if (error) {
+              throw error;
+            }
+            response.writeHead(302, {Location: `/?id=${result.insertId}`});
             response.end();
-          })
+          });
       });
     } else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
