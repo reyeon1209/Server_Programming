5686cce0bebfb25f85404be0591f8a1d14ca82c0
reyeon1209 <reyeon5368@naver.com>
2020-08-09 23:48:40 +0900

    글 수정 구현

--- Nodejs_MySQL/main.js
+++ Nodejs_MySQL/main.js
@@ -37,30 +37,29 @@ var app = http.createServer(function(request,response){
           response.end(html);
         });
       } else {
-       db.query(`SELECT * FROM topic`, function (error, results) {
+        db.query(`SELECT * FROM topic`, function(error,results){
           if (error) {
             throw error;
           }
-          db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function (error2, result) {
+          db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, result){
             if (error2) {
               throw error2;
             }
-            var title = result[0].title;
-            var description = result[0].description;
-            var list = template.list(results);
-            var html = template.HTML(title, list,
-              `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a>
-              <a href="/update?id=${queryData.id}">update</a>
-                <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${queryData.id}">
-                  <input type="submit" value="delete">
-                </form>`
-            );
-
-            response.writeHead(200);
-            response.end(html);
-            })
+           var title = result[0].title;
+           var description = result[0].description;
+           var list = template.list(results);
+           var html = template.HTML(title, list,
+             `<h2>${title}</h2>${description}`,
+             ` <a href="/create">create</a>
+                 <a href="/update?id=${queryData.id}">update</a>
+                 <form action="delete_process" method="post">
+                   <input type="hidden" name="id" value="${queryData.id}">
+                   <input type="submit" value="delete">
+                 </form>`
+           );
+           response.writeHead(200);
+           response.end(html);
+          })
         });
       }
     } else if(pathname === '/create'){
@@ -106,26 +105,31 @@ var app = http.createServer(function(request,response){
           });
       });
     } else if(pathname === '/update'){
-      fs.readdir('./data', function(error, filelist){
-        var filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
+      db.query(`SELECT * FROM topic`, function (error, results)  {
+        if (error) {
+          throw error;
+        }
+        db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function (error2, result) {
+          if (error2) {
+            throw error2;
+          }
+          var list = template.list(results);
+          var html = template.HTML(result[0].title, list,
             `
             <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                <input type="hidden" name="id" value="${result[0].id}">
+                <p><input type="text" name="title" placeholder="title" value="${result[0].title}"></p>
+                <p>
+                  <textarea name="description" placeholder="description">${result[0].description}</textarea>
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
+              </form>
+              `,
+              `<a href="/create">create</a> <a href="/update?id=${result[0].id}">update</a>`
           );
+
           response.writeHead(200);
           response.end(html);
         });
@@ -137,14 +141,11 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
+          db.query('UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?', [post.title, post.description, post.id], function(error, result) {
+            db.query('UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?', [post.title, post.description, post.id], function(error, result) {
+              response.writeHead(302, {Location: `/?id=${post.id}`});
               response.end();
-            })
+            });
           });
       });
     } else if(pathname === '/delete_process'){
