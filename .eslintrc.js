434660f7a0b2b69a11ee90a8348ed714d69d0fdb
reyeon1209 <reyeon5368@naver.com>
2020-08-16 09:39:39 +0900

    Create ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2020": true,
+		"node": true,
+	},
+	"extends": "naver",
+	"parserOptions": {
+		"ecmaVersion": 11,
+		"sourceType": "module",
+	},
+	"rules": {
+	},
+};
