55047644d4fd349aba4d7a74bead29df40eca762
reyeon1209 <reyeon5368@naver.com>
2020-08-17 23:22:25 +0900

    쿠키 옵션2

--- Cookie_Auth/cookie.js
+++ Cookie_Auth/cookie.js
@@ -14,7 +14,9 @@ http.createServer(function(request, response) {
             `Permanent=cookies; Max-Age=${60*60*24*30}`,
             // Expires : 쿠키가 언제 만료될 것인가(절대적), Max-Age : 쿠키가 얼마나 유지될 것인가 (상대적)
             'Secure=Secure; Secure',    // ; 뒤가 중요
-            'HttpOnly=HttpOnly; HttpOnly'   // ; 뒤가 중요
+            'HttpOnly=HttpOnly; HttpOnly',   // ; 뒤가 중요
+            'Path=Path; Path=/cookie',
+            'Domain=Domain; Domain=o2.org'
         ]
     })
     response.end('Cookie!!');
