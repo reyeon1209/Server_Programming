1cb1cda7a98e9482e493312ab822746226b6c17f
reyeon1209 <reyeon5368@naver.com>
2020-08-17 23:02:07 +0900

    쿠키 읽기

--- Cookie_Auth/cookie.js
+++ Cookie_Auth/cookie.js
@@ -1,7 +1,14 @@
 var http = require('http');
+var cookie = require('cookie');
+
 http.createServer(function(request, response) {
-    // response.writeHead(200, {
-    //     'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
-    // })
-    // response.end('Cookie!!');
+    var cookies = {};
+    if (request.headers.cookie !== undefined) {
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies);
+    response.writeHead(200, {
+        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+    })
+    response.end('Cookie!!');
 }).listen(3000);
